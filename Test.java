2c7e1276b102aba63d5cb9141b6a789cacee7117
hoyipyik <hoyipyik@protonmail.com>
2022-09-18 18:17:09 +0800

    18092022

--- Test.java
+++ Test.java
@@ -1,5 +1,10 @@
 public class Test{
     public static void main(String[] args) {
-        
+        int [][]mat = {
+            {1, 2},
+            { 3, 4},
+            {5,6}
+        };
+        System.out.println(mat.length);
     }
 }
