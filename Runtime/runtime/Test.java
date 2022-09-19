e2a91988b12dcf4d125bd858c6d16a77adf61c12
hoyipyik <hoyipyik@protonmail.com>
2022-09-19 21:44:15 +0800

    19092022

--- Runtime/runtime/Test.java
+++ Runtime/runtime/Test.java
@@ -1,12 +1,14 @@
 package runtime;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 public class Test{
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] holder = s.sortArrayByParity(new int[]{3,1,2,4});
-        System.out.println(holder.toString());
+        int[] holder = s.findErrorNums(new int[]{1,2,2, 4});
+        System.out.println(Arrays.toString(holder));
 
     }
 }
