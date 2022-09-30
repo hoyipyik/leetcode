1ef695113517e7e89146e4a04e2e1f5b2b10246d
hoyipyik <hoyipyik@protonmail.com>
2022-09-30 08:47:07 +0800

    290920222

---
+++ runtime/env/runtime/Test.java
@@ -0,0 +1,11 @@
+import java.security.DrbgParameters.Reseed;
+import java.util.Arrays;
+
+class Test{
+    public static void main(String[] args) {
+        // Test obj = new Test();
+        // var holder = obj.reverseString("abc");
+        String s = "  fdsf   fdf";
+        System.out.println(s.trim());
+    }
+}
